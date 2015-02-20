aa3bad1d0c086e8b94bb93dc56790353e305faf1
Keenan Lidral-Porter <klp2012@gmail.com>
2015-02-20 15:37:14 -0800

    (cleanup) moves CORS headers/body-parser into middleware.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-
 var routes = require('./config/routes.js');
 var utils = require('./config/utils.js');
 
-var app = express();
-app.use(bodyParser.json());
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-});
+var app = require('./config/middleware.js');
 
 app.post('/fetch', routes.fetch);
 app.use(utils.errorHandler);
