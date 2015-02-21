6b1ab0121e745cbd14cc8c85ef7e2f02d71f1ff6
Keenan Lidral-Porter <klp2012@gmail.com>
2015-02-20 17:44:33 -0800

    (feat) displays source code in a pretty hacky way (ver very mvp)

--- client/app/display/displayController.js
+++ client/app/display/displayController.js
@@ -1,4 +1,11 @@
 angular.module('slack.display', [])
-.controller('DisplayController', function($scope){
-  $scope.data = {};
+.controller('DisplayController', function($scope, Source){
+  $scope.data;
+
+  setTimeout(function(){
+    $scope.data = Source.displaySource();
+    console.log($scope.data);
+    $scope.$digest();
+  }, 1500)
+
 });
