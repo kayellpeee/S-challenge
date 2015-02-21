b7f286658cac25883aea6dc4013b3fab7ff86ab2
Keenan Lidral-Porter <klp2012@gmail.com>
2015-02-20 16:20:55 -0800

    (feat) serves up static html file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
+var express = require('express')
 var routes = require('./config/routes.js');
 var utils = require('./config/utils.js');
 
 var app = require('./config/middleware.js');
 
+app.use(express.static(__dirname + '/../client'));
 app.post('/fetch', routes.fetch);
 app.use(utils.errorHandler);
 
