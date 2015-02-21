4fa4a46f5dd82aa283199db123cc33b3a382509d
Keenan Lidral-Porter <klp2012@gmail.com>
2015-02-20 17:42:41 -0800

    (feat) stores result of API call in factory so data is shared b/w controllers

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,14 +1,23 @@
 angular.module('slack.services', [])
 .factory('Source', function($http){
+  var html = {};
+
   var getHTMLSource = function(data){
     return $http({
       method: 'POST',
       url: '/fetch',
       data: data
+    }).then(function(response){
+      html = response.data;
     });
   };
 
+  var displaySource = function(){
+    return html;
+  };
+
   return {
-    getHTMLSource: getHTMLSource
+    getHTMLSource: getHTMLSource,
+    displaySource: displaySource
   };
 });
