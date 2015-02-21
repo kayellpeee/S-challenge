426a3776e655c8e900dbd013aa6bc337ad1c3d56
Keenan Lidral-Porter <klp2012@gmail.com>
2015-02-20 17:43:19 -0800

    (feat) simple input form for url & reroutes to /display on submit

--- client/app/fetch/fetchController.js
+++ client/app/fetch/fetchController.js
@@ -1,4 +1,10 @@
 angular.module('slack.fetch', [])
-.controller('FetchController', function($scope){
+.controller('FetchController', function($scope, $location, Source){
   $scope.data = {};
+
+  $scope.getSource = function(){
+    Source.getHTMLSource($scope.data);
+    $location.path("/display");
+  };
+
 });
