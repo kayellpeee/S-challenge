de85c02fe63d3c0106728296cfd94ec76ef9d8db
Keenan Lidral-Porter <klp2012@gmail.com>
2015-02-20 17:59:43 -0800

    (feat) parses source code to find count of all HTML tags

--- client/app/display/displayController.js
+++ client/app/display/displayController.js
@@ -4,8 +4,34 @@ angular.module('slack.display', [])
 
   setTimeout(function(){
     $scope.data = Source.displaySource();
-    console.log($scope.data);
+    countTags($scope.data.source);
     $scope.$digest();
   }, 1500)
+
+  var countTags = function(HTMLString){
+    var tags = {};
+    var tagname;
+    var grabTagName = false;
+
+    for( var i = 0; i < HTMLString.length; i++ ){
+      if( HTMLString[i] === ">" || HTMLString[i] === " " ){
+        if( tags[tagname] ){
+          tags[tagname]++;
+        }else if( tagname !== undefined ){
+          tags[tagname] = 1;
+        }
+        grabTagName = false;
+      }
+      if( grabTagName ){
+        tagname += HTMLString[i];
+      }
+      if( HTMLString[i] === "<" && HTMLString[i + 1] !== "/" && HTMLString[i + 1] !== "!" ){
+        tagname = "";
+        grabTagName = true;
+      }
+    }
+    console.log("HERE WE GO!YAHOOO!");
+    console.log(tags);
+  };
 
 });
