bd7018d9ffa698cf87b92269f6b91e5f9eaaf0ed
Keenan Lidral-Porter <klp2012@gmail.com>
2015-02-22 14:51:18 -0800

    (feat) listens on process port, fallback to :3000

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var routes = require('./server/config/routes.js');
 var utils = require('./server/config/utils.js');
 
 var app = require('./server/config/middleware.js');
+var port = process.env.PORT || 3000;
 
 // serve up frontend and listed for POST reqs to /fetch
 app.use(express.static(__dirname + '/client'));
 app.post('/fetch', routes.fetch);
 app.use(utils.errorHandler);
 
-app.listen(3000);
+app.listen(port);
