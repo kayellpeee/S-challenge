21cf29f364e1b4071167f0e00d0a93a5bc23b00b
Keenan Lidral-Porter <klp2012@gmail.com>
2015-02-22 14:30:13 -0800

    (fix) moves server.js to root for deployment

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var routes = require('./server/config/routes.js');
+var utils = require('./server/config/utils.js');
+
+var app = require('./server/config/middleware.js');
+
+// serve up frontend and listed for POST reqs to /fetch
+app.use(express.static(__dirname + '/client'));
+app.post('/fetch', routes.fetch);
+app.use(utils.errorHandler);
+
+app.listen(3000);
