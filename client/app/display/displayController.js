0c5d2054df92ad621b1d6b79407f660dcfc298c3
Keenan Lidral-Porter <klp2012@gmail.com>
2015-02-22 13:40:04 -0800

    (feat) highlights all elements when they're clicked in the summary

--- client/app/display/displayController.js
+++ client/app/display/displayController.js
@@ -1,12 +1,25 @@
 angular.module('slack.display', [])
 .controller('DisplayController', function($scope, Storage, Parser){
+  $scope.highlight = function(tagname){
+    var elements = document.getElementsByClassName(tagname);
+
+    [].forEach.call(elements, function(element){
+      var highlightClass = " highlight";
+      var isHighlighted = element.className.indexOf(highlightClass);
+
+      if( isHighlighted > 0 ){
+        element.className = tagname;
+      }else{
+        element.className += highlightClass;
+      }
+    });
+  };
   $scope.data = Storage.data;
   $scope.parsedSourceCode = Parser.parseHTML($scope.data.source);
   $scope.tags = $scope.parsedSourceCode.tags;
 })
 .directive('parsedSourceCode', function(Storage, Parser){
   var parsedSourceCode = Parser.parseHTML(Storage.data.source);
-  console.log(parsedSourceCode.templateString)
   return {
     restrict: 'E',
     template: parsedSourceCode.templateString
